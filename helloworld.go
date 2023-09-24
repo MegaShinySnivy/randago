f28f7dc21344d71e0231211ff1de5add8bbc45ee
MegaShinySnivy <megashinysnivy@gmail.com>
2023-09-24 14:36:48 -0500

    feat: add some new features

--- helloworld.go
+++ helloworld.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func GetCreatures() []string {
@@ -15,7 +16,11 @@ func GetCreatures() []string {
 }
 
 func main() {
+	var time = time.Now()
 	var creatures = GetCreatures()
 	fmt.Println("Hello", strings.Join(creatures, ", "), "!")
 	fmt.Println(strings.Join(creatures[0:2], ", "), "and", creatures[2], "are my favorite")
+	fmt.Println(creatures[2], "and", creatures[1], "are my partners in crime!")
+	fmt.Println(creatures[0], "is a very good pawgrammer doggie")
+	fmt.Println("the current time is", time.Format("01-02-2006 15:04:05"))
 }
