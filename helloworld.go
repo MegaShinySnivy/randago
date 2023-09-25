2b40382d48c9d3237fc65a26434e6c7f825b2f40
MegaShinySnivy <megashinysnivy@gmail.com>
2023-09-25 10:25:44 -0500

    fix: cleaned up some code

--- helloworld.go
+++ helloworld.go
@@ -8,16 +8,16 @@ import (
 )
 
 func GetCreatures() []string {
-	var dog = os.Getenv("DOG")
-	var cat = os.Getenv("CAT")
-	var synx = os.Getenv("SYNX")
-	var beings = []string{dog, cat, synx}
+	dog := os.Getenv("DOG")
+	cat := os.Getenv("CAT")
+	synx := os.Getenv("SYNX")
+	beings := []string{dog, cat, synx}
 	return beings
 }
 
 func main() {
-	var time = time.Now()
-	var creatures = GetCreatures()
+	time := time.Now()
+	creatures := GetCreatures()
 	fmt.Println("Hello", strings.Join(creatures, ", "), "!")
 	fmt.Println(strings.Join(creatures[0:2], ", "), "and", creatures[2], "are my favorite")
 	fmt.Println(creatures[2], "and", creatures[1], "are my partners in crime!")
